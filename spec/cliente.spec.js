b9a45a44c292ca7b4601cb49ac23d5ccf6fa1a9d
bocunha <bocunha@hotmail.com>
2020-07-17 11:42:08 -0300

    troca do cpf

--- spec/cliente.spec.js
+++ spec/cliente.spec.js
@@ -16,21 +16,21 @@ test('Testando se o modelo cliente tem a propriedade CPF', () => {
 
   test('Testando se no modelo cliente o CPF tem 12 digitos', () => {
     let cliente = new Cliente();
-    cliente.cpf = "357.873.468-31";
+    cliente.cpf = "172.995.860-53";
 
     expect(cliente.cpf).toMatch(/^(([0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2})|([0-9]{11}))$/);
   });
 
   test('Testando se no modelo cliente o CPF e valido com ponto', () => {
     let cliente = new Cliente();
-    cliente.cpf = "357.873.468-31";
+    cliente.cpf = "172.995.860-53";
 
     expect(cliente.validateCPF()).toBe(true);
   });
 
   test('Testando se no modelo cliente o CPF e valido sem ponto', () => {
     let cliente = new Cliente();
-    cliente.cpf = "35787346831";
+    cliente.cpf = "17299586053";
 
     expect(cliente.validateCPF()).toBe(true);
   });
